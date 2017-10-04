546c98a7f3bec64d2f4c12bf5f697dd79f1dc81a
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-04 20:54:52 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
 
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+
+  contains_false = false
+
+  j = 0
+  while j < block_return_values.length
+    if block_return_values[j] == false
+      contains_false = true
+    end
+    j += 1
+  end
+
+  !contains_false
 end
